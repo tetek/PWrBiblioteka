dcbe5c151469f6eae52122eff83ce8b5029ef3ed
Tetek <tetek1@gmail.com>
2012-12-17 18:40:43 +0100

    renaming and stuff

--- Pwr/AppDelegate.m
+++ Pwr/AppDelegate.m
@@ -21,8 +21,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    NSArray *results = [BookListFetcher fetchBooksForQuery:@"analiza"];
-    
+        
     self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
     // Override point for customization after application launch.
     self.viewController = [[[ViewController alloc] initWithNibName:@"ViewController" bundle:nil] autorelease];
